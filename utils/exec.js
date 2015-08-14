28bda1819037179a93af928f3f563237e8714632
Aram Zadikian <brancusi@gmail.com>
2015-08-14 13:20:14 -0700

    Cleanup

--- utils/exec.js
+++ utils/exec.js
@@ -2,17 +2,15 @@ var exec = require('child_process').exec,
     Promise = require('promise'),
     spawn = require('child_process').spawn;
 
-module.exports = ExecTools;
-
-ExecTools.prototype.runExec = function(cmd){
-  return new Promise(function(resolve, reject){
-    var child = exec(cmd,
-      function (error, stdout, stderr) {
-        resolve(stdout);
-        if (error !== null) {
-          reject(error);
-        }
-    });
-
-  });
+module.exports = {
+  runExec: function(cmd){
+    return new Promise(function(resolve, reject){
+      var child = exec(cmd,
+        function (error, stdout, stderr) {
+          resolve(stdout);
+          if (error !== null) {
+            reject(error);
+          }
+      });
+  }
 }
